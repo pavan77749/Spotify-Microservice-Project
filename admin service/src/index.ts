2d3a2de6c8de0084b23b779ec6c01c84ba7392cb
pavan77749 <pavangpay05@gmail.com>
2025-05-23 20:08:31 +0530

    feat: initialize admin service with TypeScript, Express, and dotenv
    - Added package.json with dependencies and scripts for building and running the service.
    - Created index.ts as the entry point for the Express application.
    - Configured tsconfig.json for TypeScript compilation settings.

---
+++ admin service/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
