16dc4892edd755ae3723c5632f4df2b19d602e77
pavan77749 <pavangpay05@gmail.com>
2025-05-26 22:38:38 +0530

    feat: Initialize database and add cloudinary configuration
    - Added database initialization logic to create albums and songs tables.
    - Configured cloudinary with environment variables.
    - Introduced TryCatch middleware for error handling in controllers.
    - Implemented data URI parser for file uploads.
    - Set up user authentication middleware using axios to verify tokens.
    - Created basic route structure for the admin service.
    - Updated package.json to include new dependencies for cloudinary and datauri.

---
+++ admin service/src/controller.ts
@@ -0,0 +1,6 @@
+import TryCatch from "./TryCatch.js";
+
+
+export const addAlbum = TryCatch(async (req, res) => {
+  
+});
--- admin service/src/index.ts
+++ admin service/src/index.ts
@@ -1,13 +1,53 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { sql } from './config/db.js';
+import adminRoutes from './route.js';
+import cloudinary from 'cloudinary';
+
 
 dotenv.config();
 
+cloudinary.v2.config({
+    cloud_name: process.env.Cloud_Name,
+    api_key: process.env.Cloud_Api_Key,
+    api_secret: process.env.Cloud_Api_Secret,
+});
 
 const app = express();
 
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS albums (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(500) NOT NULL,
+            thumbnail VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+         await sql`CREATE TABLE IF NOT EXISTS songs (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(500) NOT NULL,
+            thumbnail VARCHAR(255) ,
+            audio VARCHAR(255) NOT NULL,
+            album_id INTEGER REFERENCES albums(id) ON DELETE CASCADE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`
+        console.log('Database initialized successfully');
+    } catch (error) {
+        console.error('Error initializing database:', error);
+        process.exit(1);
+    }
+}
+
+
+app.use("/api/v1",adminRoutes)
+
 const port = process.env.PORT || 7000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+initDB().then(() => {
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 });
